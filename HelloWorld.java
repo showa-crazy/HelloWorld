fba1b92a66baf22ef1b51c4416a802decf9a36bd
showa-crazy <showa.crazy@gmail.com>
2012-08-28 20:18:31 +0900

    修正してみました。
    どうでしょうか。

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld {
+	
+	public static final int MAX_HELLO = 10;
 	public static void main(String args[]){
 		System.out.println("Hello World!");
+		for( int idx = 1 ; idx <= MAX_HELLO ; idx++ ){
+			System.out.println("Hello " + idx);
+		}
 	}
 }
